4bf169d7eb1a7b8dc12edb50f28b269b58b9b57f
Jose Ricardo de Oliveira Damico <jd.comment@gmail.com>
2011-04-15 19:35:24 -0300

    Building basic architecture

--- serviceproject-web/src/org/jdamico/serviceproject/servlets/Dashboard.java
+++ serviceproject-web/src/org/jdamico/serviceproject/servlets/Dashboard.java
@@ -1,6 +1,8 @@
 package org.jdamico.serviceproject.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,21 +19,86 @@ public class Dashboard extends HttpServlet {
      */
     public Dashboard() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		
+		
+		
+		
+		
+		String content0 = 	"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" +
+							"<html xmlns=\"http://www.w3.org/1999/xhtml\">" +
+							"<head>" +
+							"    <title>ServiceProject Dashboard</title>\n" +
+							"    <script src=\"js/jquery-1.3.2.debug.js\" type=\"text/javascript\"></script>\n" +
+							"    <script src=\"js/json.htmTable.js\" type=\"text/javascript\"></script>\n" +
+							"    <script src=\"js/json.debug.js\" type=\"text/javascript\"></script>\n" +
+							"    <script src=\"js/serviceproject.js\" type=\"text/javascript\"></script>\n" +
+								"    <link href=\"css/default.css\" rel=\"stylesheet\" type=\"text/css\" />\n" +
+							"    <script type=\"text/javascript\">\n" + 
+							
+
+							"$(document).ready(function() {\n" +
+							"	    $.ajax({" +
+							"       type: \"GET\"," +
+							"       url: \"ReadProjects\"," +
+							"	    contentType: \"application/json; charset=utf-8\"," +
+							"       dataType: \"json\"," +
+							"       success: function(res) {" +
+							"		$('#DynamicGridLoading').hide();" +
+							"		$('#Results').append(CreateTableView(res.serviceproject.projects, \"lightPro\", true,1, \"getTasks\", \"id\",\"name\")).fadeIn();" +
+							"       }" +
+							"	    });" +
+							"		});" +		
+							
+							"    </script>\n" +
+							"</head>\n";
+		
+		
+		String forms = 		"<div id='newProject'>New project:\n" +
+							"<form id='newProjectForm' action='ReadProjects'>\n" +
+							"<table>\n" +
+							"<tr><td>Project Name:</td><td><input type='text' name='pname'></td></tr>\n" +
+							"<tr><td>Customer:</td><td><input type='text' name='customer'></td></tr>\n" +
+							"<tr><td>&nbsp</td><td><input type='submit' onclick=\"postForm('#newProjectForm', '#newProject')\"></td></tr>\n" +
+							"</table>\n" +
+							"</form>\n" +
+							"</div>\n";
+		
+		
+		String result = 	"<div id='result'>.....</div>\n";
+		
+		
+		String content1 = 	"<body onload='bodyLoad()'>\n" +
+							"<div id='title'></div><hr>" + forms + result +
+							"<form id=\"form1\" >\n" +
+							"	<div id=\"tasks\"></div>" +
+							"    <div id=\"Results\" >\n" +
+							"        <div id=\"DynamicGridLoading\" >\n" +
+							"            <img src=\"images/loading.gif\" /><span> Loading Data... </span>\n" +
+							"        </div>\n" +
+							"    </div>\n" +
+							"    </form>\n" +
+							"<hr>" +
+							"serviceproject 0.1" +
+							" | <a href='#' onclick='newProject()'>New Project</a> </body>\n" +
+							"</html>\n";
+		
+		out.println(content0);
+		out.println(content1);
+	
 	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 }
--- serviceproject-web/src/org/jdamico/serviceproject/servlets/ReadProjects.java
+++ serviceproject-web/src/org/jdamico/serviceproject/servlets/ReadProjects.java
@@ -1,10 +1,19 @@
 package org.jdamico.serviceproject.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jdamico.serviceproject.dataobjects.Project;
+import org.jdamico.serviceproject.ejbs.ReadProjectsLocal;
 
 /**
  * Servlet implementation class ReadProjects
@@ -23,14 +32,54 @@ public class ReadProjects extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		
+		
+		String json = null;
+		
+		try{
+			Context context = new InitialContext();
+			ReadProjectsLocal rpl = (ReadProjectsLocal) context.lookup("java:comp/env/ejb/ReadProjects");
+			json = rpl.getProjectsJSON();
+		}catch(NamingException ne){
+			
+		}
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		
+		out.println(json);
+		out.flush();
+		out.close();
+		
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		
+		String name = request.getParameter("pname");
+		String customer = request.getParameter("customer");
+		
+		Project project = new Project(name, customer, new Date());
+		
+		String json = null;
+		
+		try{
+			Context context = new InitialContext();
+			ReadProjectsLocal rpl = (ReadProjectsLocal) context.lookup("java:comp/env/ejb/ReadProjects");
+			rpl.addProject(project);
+		}catch(NamingException ne){
+			
+		}
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		
+		out.println(json);
+		out.flush();
+		out.close();
 	}
 
 }
